360e3c43e192d36d4011910e2296665bbbb04326
Rishabhdaliya <14rishabhdaliya@gmail.com>
2023-09-15 21:49:22 +0530

    Inital commit

---
+++ test.js
@@ -0,0 +1,30 @@
+const months = ['Jan', 'March', 'April', 'June', 'july'];
+
+// Array.prototype.splice()
+
+// Array.splice(index, no of elements to be replaced, what you want to add in place of the replacement )
+
+// months.splice(1, 3, "Feb" )
+
+// Array.map((curentElement) => { })
+
+// months.map((data) =>
+// {console.log(data);
+// }
+// )
+
+// const avg =[99, 7, 9, 67, 7];
+
+// // Array.filter((curentElement) => perform the condition)
+
+// const newAvg =  avg.filter((name) => name === 7)
+
+// console.log(newAvg);
+
+// const avg =[10, 10, 10];
+
+// const initialValue= 1;
+
+// const result   = avg.reduce((accumulator, currentValue ) => accumulator * currentValue, initialValue)
+
+// console.log(result);
