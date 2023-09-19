567297bc23ba596ec68b15916e12b16f2e69e545
Rishabhdaliya <14rishabhdaliya@gmail.com>
2023-09-19 21:45:49 +0530

    feat: simple conditional statements

---
+++ Conditional statement/simpleConditions.js
@@ -0,0 +1,63 @@
+//? Conditonal statements
+
+// if
+
+// if (condition) {
+
+// }
+
+// if (4 != 4) {
+//   console.log('I am inside');
+// }
+
+// if else
+
+// if (4 != 4) {
+//   console.log('I am inside if');
+// } else {
+//   console.log('I am inside else');
+// }
+
+// nested if
+// if (4 != 4) {
+//   console.log('inside  1st if');
+//   if (5 === 5) {
+//     console.log('inside nested');
+//   }
+// }
+
+// let a = 100;
+// if (a > 5 && a < 15) {
+//   console.log('In range');
+// }
+
+// if, else if, else
+
+// if (a > 4) {
+
+// } else if(a > 4 && a < 14){
+
+// }
+// else if(a > 4 && a < 14){
+
+// } else if(a > 4 && a < 14){
+
+// }else{
+
+// }
+
+// ? switch case
+// switch ('b') {
+//   case 'add':
+//     // a = b+ c;
+//     break;
+//   case 'sub':
+//     // a = b -c;
+//     break;
+//   case 'c':
+//     console.log('case c');
+//     break;
+//   default:
+//     console.log('Invaid option');
+//     break;
+// }
