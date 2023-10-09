ba038b8dd622967643b38ded497944f0aaecfc66
Rishabhdaliya <14rishabhdaliya@gmail.com>
2023-10-09 21:40:26 +0530

    feat: game

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 'use strict';
 
-let secretValue = 0;
+var secretValue = 0;
 
 // generate random number
 function getRandomNumber() {
   // Use Math.floor to round down to the nearest integer
-  secretValue = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
-
+  secretValue = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
   return secretValue;
 }
+
+getRandomNumber();
 
 // checking wheather the number guesed  is equa;l to secret number or not
 function submitHandle() {
   const guessedNumber = parseInt(document.querySelector('.guess').value);
   if (guessedNumber === secretValue) {
-    document.querySelector('.secretNumber').textContent = getRandomNumber();
-    return alert('Congrats guessed right number');
+    document.querySelector('.secretNumber').textContent = secretValue;
+    document.querySelector('#description').textContent =
+      'Congrats you Won the Game';
   } else {
     alert('OPPS! guessed wrong number!');
   }
 }
 
 //heading
-document.querySelector('#description').textContent = 'Guess the Number';
-
-// secret number assigned
-document.querySelector('.secretNumber').textContent = getRandomNumber();
+document.querySelector('#description').textContent =
+  'Guess the Number between 1-10';
 
 // click event
 document.querySelector('.action').addEventListener('click', submitHandle);
+
+console.log(secretValue);
+// New game event
+
+function newGameHandler() {
+  getRandomNumber();
+  return (
+    (document.querySelector('.secretNumber').textContent = ''),
+    (document.querySelector('.guess').value = '')
+  );
+}
+
+document.querySelector('.newSecret').addEventListener('click', newGameHandler);
